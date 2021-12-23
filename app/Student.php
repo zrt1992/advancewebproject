b8f8dd789155b819458a867fa4043bd3daaea049
Zulfiqar Tariq <zulfiqar_tariq@hotmail.com>
2021-12-23 12:41:05 +0500

    changes in files

--- app/Student.php
+++ app/Student.php
@@ -5,11 +5,13 @@ include 'database.php';
 $db_connect = db_connect();
 
 function getUserCourses($db_connect){
-    $sql = "SELECT * FROM user as u INNER JOIN role as r on u.role_id=r.id INNER JOIN users_courses as uc on uc.user_id=u.id INNER JOIN course as c  on c.id=uc.course_id WHERE u.id=1 and r.id=1";
+    $sql = "SELECT * FROM user as u
+    INNER JOIN role as r on u.role_id=r.id INNER JOIN users_courses as uc on uc.user_id=u.id INNER JOIN course as c  on c.id=uc.course_id WHERE u.id=1 and r.id=1";
     $result = $db_connect->query($sql);
     var_dump($result);
 
 }
+
 function getUser($db_connect){
 
 }
